97780629710f33040486df8294984e8008fa98a3
William Sykes <wt04s12@gmail.com>
2023-11-03 01:40:52 -0700

    Added DB

---
+++ coffee_shop_database.sql
@@ -0,0 +1,26 @@
+DROP DATABASE IF EXISTS my_coffee_shop;
+CREATE DATABASE my_coffee_shop;
+USE my_coffee_shop;
+
+CREATE TABLE CoffeeShopOrders(
+	Fname VARCHAR(50),
+    Lname VARCHAR(50),
+    OrderDescription MEDIUMTEXT,
+    Price DOUBLE
+);
+
+INSERT INTO CoffeeShopOrders VALUES ("Bob", "Smith", "Lattee with a cookie", 6.50);
+
+
+CREATE TABLE ManagerView (
+	Product VARCHAR(1000),
+    Price double,
+    Quantity INT,
+    Category VARCHAR(50)
+);
+
+INSERT INTO ManagerView VALUES ("CoffeeGrounds", 50, NULL, "Inventory");
+INSERT INTO ManagerView VALUES ("Lattee", NULL, 3.50, "Price Check");
+
+SELECT * FROM my_coffee_shop.managerview;
+SELECT * FROM my_coffee_shop.coffeeshoporders;
