6951dd945bc26372f09c6141fc891a9eb7e14304
William Sykes <wt04s12@gmail.com>
2023-11-21 00:06:00 -0800

    Added information display for all buttons and boxes, refined gui

--- EmployeeProductView.java
+++ EmployeeProductView.java
@@ -7,53 +7,50 @@ import java.sql.Statement;
 public class EmployeeProductView {
 	
 	public static Connection con = null;
+	
 	public static void connection() {
-		String url = "jdbc:mysql://localhost:3306/smcoffee";
+		String url = "jdbc:mysql://localhost:3306/my_coffee_shop";
 		String username = "cs380Will";
 		String pass = "cs380";
 		
 		try {
-			con = DriverManager.getConnection(url, username,pass);
+			con = DriverManager.getConnection(url, username, pass);
 			System.out.println("Connected");
 		}catch(Exception e) {
 			System.out.println(e.getMessage());
 		}
 }
 	
-	public static void addProduct(String product, Double price, int quantity, String category) {
+	public static boolean addProduct(String product, Double price, int quantity, String category) {
 	
-	/**
-     * Method to add a row in manageview table
-     * @param String dvdT Title from field 
-     * @param String dvdY Year  from field
-     * @param Integer dvdCN Call number from field
-     */
-	try {
-        Statement st = con.createStatement();
+		/**
+		 * Method to add a row in manageview table
+		 * @param String dvdT Title from field 
+		 * @param String dvdY Year  from field
+		 * @param Integer dvdCN Call number from field
+		 */
+		try {
+			Statement st = con.createStatement();
 
         
-        String query = "INSERT INTO smcoffee.managerview (Product, Price, Quantity, Category) " +
-                "VALUES ('" + product + "', '" + price + "', '" + quantity + "', '" + category +"')";
+			String query = "INSERT INTO my_coffee_shop.managerview (Product, Price, Quantity, Category) " +
+					"VALUES ('" + product + "', '" + price + "', '" + quantity + "', '" + category +"')";
 
-        int rowsAffected = st.executeUpdate(query);
+			int rowsAffected = st.executeUpdate(query);
 
-        if (rowsAffected > 0) {
-        	System.out.println("---------------------------------------");
-            System.out.println("Product added successfully!");
-            System.out.println("---------------------------------------");
-        } else {
-        	System.out.println("---------------------------------------");
-            System.out.println("Failed to add product.");
-            System.out.println("---------------------------------------");
-        }
-    } catch (SQLException e) {
-        e.printStackTrace();  
-    
-    }
+			if (rowsAffected > 0) {
+				return true;
+			} else {
+				return false;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();  
+			return false;
+		}
 	
 	}
 	
-	public static void removeProduct(String product) {
+	public static boolean removeProduct(String product) {
 		
 		/**
          * Method to remove a row in books table
@@ -63,67 +60,61 @@ public class EmployeeProductView {
 	        Statement st = con.createStatement();
 
 	        
-	        String query = "DELETE FROM smcoffee.managerview WHERE Product = '" + product + "'";
+	        String query = "DELETE FROM my_coffee_shop.managerview WHERE Product = '" + product + "'";
 
 	        int rowsAffected = st.executeUpdate(query);
 
 	        if (rowsAffected > 0) {
-	        	System.out.println("---------------------------------------");
-	            System.out.println("Product removed successfully!");
-	            System.out.println("---------------------------------------");
-	        } else {
-	        	System.out.println("---------------------------------------");
-	            System.out.println("Product not found or failed to remove.");
-	            System.out.println("---------------------------------------");
-	        }
-	    } catch (SQLException e) {
-	        e.printStackTrace();  
-	    }
+				return true;
+			} else {
+				return false;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();  
+			return false;
+		}
 		
 		
 	}
 	
 	
-	public static void modProduct(String product, Double price, int quantity, String category) {
+	public static boolean modProduct(String product, Double price, int quantity, String category) {
 		/**
-         * Method to remove a row in books table
+         * Method to modify a row in books table
          * @param String bookTitle Title from field 
          */
 	    try {
 	        Statement st = con.createStatement();
 
 	        
-	        String updateStatusQuery = "UPDATE smcoffee.managerview SET Price = '"+ price +"' WHERE Product = '" + product + "'";
+	        String updateStatusQuery = "UPDATE my_coffee_shop.managerview SET Price = '"+ price +"' WHERE Product = '" + product + "'";
 	        int rowsAffected = st.executeUpdate(updateStatusQuery);
-	        String updateStatusQuery2 = "UPDATE smcoffee.managerview SET Quantity = '"+ quantity +"' WHERE Product = '" + product + "'";
+	        String updateStatusQuery2 = "UPDATE my_coffee_shop.managerview SET Quantity = '"+ quantity +"' WHERE Product = '" + product + "'";
 	        int rowsAffected2 = st.executeUpdate(updateStatusQuery2);
-	        String updateStatusQuery3 = "UPDATE smcoffee.managerview SET Category = '"+ category +"' WHERE Product = '" + product + "'";
+	        String updateStatusQuery3 = "UPDATE my_coffee_shop.managerview SET Category = '"+ category +"' WHERE Product = '" + product + "'";
 	        int rowsAffected3 = st.executeUpdate(updateStatusQuery2);
 
 	        if (rowsAffected3 > 0) {
-	        	System.out.println("---------------------------------------");
-	            System.out.println("Product modified successfully!");
-	            System.out.println("---------------------------------------");
-	        } else {
-	        	System.out.println("---------------------------------------");
-	            System.out.println("Product not found or failed to modify.");
-	            System.out.println("---------------------------------------");
-	        }
-	    } catch (SQLException e) {
-	        e.printStackTrace();  
-	    }
+				return true;
+			} else {
+				return false;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();  
+			return false;
+		}
 	}
-	
-	public static void searchProduct(String product) {
-		/**
-         * Search and retrieve customer information
-         * @param String custName from field
-         */
+	/**
+     * Search and retrieve customer information
+     * @param String custName from field
+     */
+	public static String searchProduct(String product) {
+		
 	    try {
 	        Statement st = con.createStatement();
 
 	        // query to retrieve customer information
-	        String selectQuery = "SELECT * FROM smcoffee.managerview WHERE Product = '" + product + "'";
+	        String selectQuery = "SELECT * FROM my_coffee_shop.managerview WHERE Product = '" + product + "'";
 	        ResultSet resultSet = st.executeQuery(selectQuery);
 
 	        // if the customer with the provided name exists
@@ -134,21 +125,66 @@ public class EmployeeProductView {
 	            int quantity = resultSet.getInt("Quantity");
 	            String category = resultSet.getString("Category");
 
-	            // Print the customer information 
-	            System.out.println("----------------------------");
-	            System.out.println("Product Information:");
-	            System.out.println("Product Name: " + Product);
-	            System.out.println("Price: " + price);
-	            System.out.println("Quantity: " + quantity);
-	            System.out.println("Category: " + category);
-	            System.out.println("----------------------------");
+	         // Build the product information string
+	            StringBuilder result = new StringBuilder();
+	            result.append("Product Information:\n");
+	            result.append("Name: ").append(Product).append("\n");
+	            result.append("Price: $").append(price).append("\n");
+	            result.append("Quantity: ").append(quantity).append("\n");
+	            result.append("Category: ").append(category).append("\n");
+
+	            return result.toString();
 	        } else {
-	        	System.out.println("----------------------------");
-	            System.out.println("Item not found.");
-	            System.out.println("----------------------------");
+	        	return "Item not found.";
 	        }
 	    } catch (SQLException e) {
-	        e.printStackTrace();  
+	    	e.printStackTrace();
+	        return "Error occurred while searching for the product.";
+	    }
+	    
+	    
+	}
+	
+	
+	/**
+     * Search and retrieve customer information
+     * @param String custName from field
+     */
+	public static String searchCat(String category) {
+		
+		try {
+	        Statement st = con.createStatement();
+
+	        // query to retrieve product information for the specified category
+	        String selectQuery = "SELECT * FROM my_coffee_shop.managerview WHERE Category = '" + category + "'";
+	        ResultSet resultSet = st.executeQuery(selectQuery);
+
+	        StringBuilder result = new StringBuilder();
+
+	        // loop through the result set for each product in the category
+	        while (resultSet.next()) {
+	            // Retrieve information for each product
+	            String product = resultSet.getString("Product");
+	            double price = resultSet.getDouble("Price");
+	            int quantity = resultSet.getInt("Quantity");
+
+	            // Append the product information to the result StringBuilder
+	            result.append("Product Information:\n");
+	            result.append("Name: ").append(product).append("\n");
+	            result.append("Price: $").append(price).append("\n");
+	            result.append("Quantity: ").append(quantity).append("\n");
+	            result.append("----------------------------\n");
+	        }
+
+	        // Check if any products were found
+	        if (result.length() > 0) {
+	            return result.toString();
+	        } else {
+	            return "No products found in the specified category.";
+	        }
+	    } catch (SQLException e) {
+	        e.printStackTrace();
+	        return "Error occurred while searching for products in the category.";
 	    }
 	}
 }
--- productView.java
+++ productView.java
@@ -4,11 +4,14 @@ import java.awt.event.MouseEvent;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+
 import java.awt.BorderLayout;
 import javax.swing.JButton;
 import javax.swing.SwingConstants;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
@@ -33,6 +36,7 @@ public class productView {
 				try {
 					productView window = new productView();
 					window.frame.setVisible(true);
+					EmployeeProductView.connection();
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -52,18 +56,46 @@ public class productView {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 570, 414);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
 		JPanel panel = new JPanel();
-		panel.setBounds(10, 11, 116, 128);
+		panel.setBounds(20, 70, 106, 138);
 		frame.getContentPane().add(panel);
 		
+		JTextArea textArea = new JTextArea();
+		textArea.setBounds(154, 11, 390, 353);
+		frame.getContentPane().add(textArea);
+		
+		JScrollPane scrollPane = new JScrollPane(textArea);
+        scrollPane.setBounds(154, 11, 390, 353);
+        frame.getContentPane().add(scrollPane);
+		
 		final JComboBox<String> coffee = new JComboBox(new String[] {"", "Price Check", "Inventory"});
-        coffee.setBounds(20, 200, 116, 40);
+		coffee.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                // This method will be called when an item is selected in the JComboBox
+                String selectedOption = (String) coffee.getSelectedItem();
+             
+                String productInfo =  selectedOption + ":" + "\n" + EmployeeProductView.searchCat(selectedOption);
+
+		        // Update the GUI component with the product information
+		        textArea.setText(productInfo);
+
+		        // If the product was not found, show an error message
+		        if (productInfo.equals("Items not found.")) {
+		            JOptionPane.showMessageDialog(null, "Could not find items.", "Error", JOptionPane.ERROR_MESSAGE);
+		        }
+                // Add your custom code here based on the selected item
+            }
+        });
+		coffee.setBounds(10, 36, 116, 23);
         frame.getContentPane().add(coffee);
+		
+        
 		
 		JButton searchButton = new JButton("Search");
 		searchButton.addMouseListener(new MouseAdapter() {
@@ -72,7 +104,14 @@ public class productView {
 				
 				prodField = productField.getText();
 				
-				EmployeeProductView.searchProduct(prodField);
+				String productInfo = EmployeeProductView.searchProduct(prodField);
+				
+				textArea.setText(productInfo);
+			
+				if (productInfo.equals("Item not found.")) {
+		            JOptionPane.showMessageDialog(null, "Could not find product.", "Error", JOptionPane.ERROR_MESSAGE);
+		        }
+				
 			}
 		});
 		panel.add(searchButton);
@@ -81,16 +120,18 @@ public class productView {
 		panel.add(modButton);
 		modButton.addMouseListener(new MouseAdapter() {
 			@Override
-			public void mouseClicked(MouseEvent e) {
-				
-				prodField = productField.getText();
-				priField = Double.parseDouble(priceField.getText());
-				quaField = Integer.parseInt(quantityField.getText());
-				String selectedValue = (String) coffee.getSelectedItem();
-				
-				
-				EmployeeProductView.modProduct(prodField, priField, quaField, selectedValue);
-				
+		    public void mouseClicked(MouseEvent e) {
+		        prodField = productField.getText();
+
+		        String productInfo = EmployeeProductView.searchProduct(prodField);
+
+		        // Update the GUI component with the product information
+		        textArea.setText(productInfo);
+
+		        // If the product was not found, show an error message
+		        if (productInfo.equals("Item not found.")) {
+		            JOptionPane.showMessageDialog(null, "Could not find Product.", "Error", JOptionPane.ERROR_MESSAGE);
+		        }
 			}
 		});
 		
@@ -107,8 +148,14 @@ public class productView {
 				quaField = Integer.parseInt(quantityField.getText());
 				String selectedValue = (String) coffee.getSelectedItem();
 				
+				boolean productAdded = EmployeeProductView.addProduct(prodField, priField, quaField, selectedValue);
 				
-				EmployeeProductView.addProduct(prodField, priField, quaField, selectedValue);
+				if (productAdded) {
+		            JOptionPane.showMessageDialog(null, "Product added successfully!", "Success", JOptionPane.INFORMATION_MESSAGE);
+		        } else {
+		            JOptionPane.showMessageDialog(null, "Failed to add product.", "Error", JOptionPane.ERROR_MESSAGE);
+		        }
+				
 				
 			}
 		});
@@ -120,49 +167,54 @@ public class productView {
 				
 				prodField = productField.getText();
 				
-				EmployeeProductView.removeProduct(prodField);
+				boolean productremoved = EmployeeProductView.removeProduct(prodField);
 				
+				if (productremoved) {
+		            JOptionPane.showMessageDialog(null, "Product removed successfully!", "Success", JOptionPane.INFORMATION_MESSAGE);
+		        } else {
+		            JOptionPane.showMessageDialog(null, "Failed to remove product.", "Error", JOptionPane.ERROR_MESSAGE);
+		        }
 			}
 		});
 		panel.add(reButton);
 		
 		JLabel lblNewLabel = new JLabel("Product");
-		lblNewLabel.setBounds(148, 140, 46, 14);
+		lblNewLabel.setBounds(10, 209, 46, 14);
 		frame.getContentPane().add(lblNewLabel);
 		
 		JLabel lblNewLabel_1 = new JLabel("Quantity:");
-		lblNewLabel_1.setBounds(148, 176, 46, 14);
+		lblNewLabel_1.setBounds(10, 251, 81, 14);
 		frame.getContentPane().add(lblNewLabel_1);
 		
 		JLabel lblNewLabel_2 = new JLabel("Price:");
-		lblNewLabel_2.setBounds(148, 210, 62, 20);
+		lblNewLabel_2.setBounds(10, 298, 62, 20);
 		frame.getContentPane().add(lblNewLabel_2);
 		
-		JTextArea textArea = new JTextArea();
-		textArea.setBounds(136, 11, 288, 109);
-		frame.getContentPane().add(textArea);
+		
 		
 		productField = new JTextField();
-		productField.setBounds(231, 137, 193, 20);
+		productField.setBounds(61, 209, 86, 20);
 		frame.getContentPane().add(productField);
 		productField.setColumns(10);
 		
 		quantityField = new JTextField();
-		quantityField.setBounds(231, 173, 193, 20);
+		quantityField.setBounds(61, 248, 86, 20);
 		frame.getContentPane().add(quantityField);
 		quantityField.setColumns(10);
 		
 		priceField = new JTextField();
-		priceField.setBounds(231, 213, 193, 20);
+		priceField.setBounds(61, 298, 86, 20);
 		frame.getContentPane().add(priceField);
 		priceField.setColumns(10);
 		
 		JLabel lblNewLabel_3 = new JLabel("Category Select:");
-		lblNewLabel_3.setBounds(20, 176, 106, 14);
+		lblNewLabel_3.setBounds(10, 11, 106, 14);
 		frame.getContentPane().add(lblNewLabel_3);
 		
 		JButton clsWButton = new JButton("Close Window");
-		clsWButton.setBounds(20, 150, 106, 23);
+		clsWButton.setBounds(10, 341, 106, 23);
 		frame.getContentPane().add(clsWButton);
+		
+		
 	}
 }
