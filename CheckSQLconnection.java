706a89fed2c189fa45c19ca04a2f2546e844316c
DevlinHamill <devlinswork@yahoo.com>
2023-11-21 10:59:38 -0800

    modified CheckSQLconnection.java to improve user readability

--- CheckSQLconnection.java
+++ CheckSQLconnection.java
@@ -1,48 +1,81 @@
 import java.sql.DriverManager;
+
+import javax.swing.JOptionPane;
+
 import java.sql.*;
 
 public class CheckSQLconnection {
+	/**
+	 * a url string to check its connection later on
+	 */
 	private static String url;
+	/**
+	 * creates a string username to hold the sql database username
+	 */
 	private static String username;
+	/**
+	 * creates a string password that will hold the sql database password 
+	 */
 	private static String password;
-	
-	public CheckSQLconnection(String url, String username, String password){
-		this.url = url;
-		this.username = username;
-		this.password = password;
-	}
-		
+
+	/**
+	 * updates the url within the object
+	 * @param tempURL the current string url
+	 */
 	public static void setURL(String tempURL) {
 		url = tempURL;
 	}
 	
+	/**
+	 * retrieves the url to the sql database
+	 * @return the url the string url of the database
+	 */
 	public static String getURL() {
 		return url;
 	}
 	
+	/**
+	 * updates the username to the mysql database
+	 * @param tempUsername new username to the database
+	 */
 	public static void setUsername(String tempUsername) {
 		username = tempUsername;
 	}
 	
+	/**
+	 * retrieves the current instance of the username as a string
+	 * @return string that holds the username to the mySQL connection
+	 */
 	public static String getUsername() {
 		return username;
 	}
 	
+	/**
+	 * updates the password the the current instance of the mySQL database
+	 * @param tempPassword new string for the password
+	 */
 	public static void setPassword(String tempPassword) {
 		password = tempPassword;
 	}
 	
+	/**
+	 * retrieves the password the sql database
+	 * @return the password as a string
+	 */
 	public static String getPassword() {
 		return password;
 	}
 	
+	/**
+	 * checks the mySQL connection with the given values and returns a boolean
+	 * @return a boolean that tells if the connection is valid or not
+	 */
 	public static boolean connection() {
 	
 		try(Connection con = DriverManager.getConnection(url, username, password)) {
-			System.out.println("connected");
 			return true;
 		}catch(SQLException e){
-			System.out.println("exception "+ e.getMessage());
+			JOptionPane.showMessageDialog(null, e.getMessage(), "SQL connection Issue", JOptionPane.ERROR_MESSAGE);
 			return false;
 		}
 	}
